6a8ff6b021997ace4efd8d1176bac6077116819f
UditKarth <uditk3@illinois.edu>
2024-03-24 04:42:51 -0500

    matching

---
+++ matching/bootstrap.go
@@ -0,0 +1,25 @@
+package matching
+
+import (
+	"github.com/gitbitex/gitbitex-spot/conf"
+	"github.com/gitbitex/gitbitex-spot/service"
+	"github.com/siddontang/go-log/log"
+)
+
+func StartEngine() {
+	gbeConfig := conf.GetConfig()
+
+	products, err := service.GetProducts()
+	if err != nil {
+		panic(err)
+	}
+	for _, product := range products {
+		orderReader := NewKafkaOrderReader(product.Id, gbeConfig.Kafka.Brokers)
+		snapshotStore := NewRedisSnapshotStore(product.Id)
+		logStore := NewKafkaLogStore(product.Id, gbeConfig.Kafka.Brokers)
+		matchEngine := NewEngine(product, orderReader, logStore, snapshotStore)
+		matchEngine.Start()
+	}
+
+	log.Info("match engine ok")
+}
---
+++ matching/kafka_order_reader.go
@@ -0,0 +1,47 @@
+package matching
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gitbitex/gitbitex-spot/models"
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	TopicOrderPrefix = "matching_order_"
+)
+
+type KafkaOrderReader struct {
+	orderReader *kafka.Reader
+}
+
+func NewKafkaOrderReader(productId string, brokers []string) *KafkaOrderReader {
+	s := &KafkaOrderReader{}
+
+	s.orderReader = kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   brokers,
+		Topic:     TopicOrderPrefix + productId,
+		Partition: 0,
+		MinBytes:  1,
+		MaxBytes:  10e6,
+	})
+	return s
+}
+
+func (s *KafkaOrderReader) SetOffset(offset int64) error {
+	return s.orderReader.SetOffset(offset)
+}
+
+func (s *KafkaOrderReader) FetchOrder() (offset int64, order *models.Order, err error) {
+	message, err := s.orderReader.FetchMessage(context.Background())
+	if err != nil {
+		return 0, nil, err
+	}
+
+	err = json.Unmarshal(message.Value, &order)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return message.Offset, order, nil
+}
